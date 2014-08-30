08fdd21a3d4684afdb1c1660f7e50a7ad1b8d810
jibreel <jibbo88@gmail.com>
2014-08-30 16:30:06 +0300

    defined forum blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, session
+from app.forum import forum
 
 
 app = Flask('app')
 app.config.from_object('config')
+app.register_blueprint(forum, url_prefix='/forum')
 
 
 @app.route('/')
--- app/forum/views.py
+++ app/forum/views.py
@@ -2,3 +2,8 @@ from flask import Blueprint, request, session, g, redirect, url_for, abort, \
                   render_template, flash, make_response
 
 
+forum = Blueprint('forum', __name__)
+
+@forum.route('/')
+def index():
+    return 'forum index page'
