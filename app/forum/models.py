9bde49e5ba7d5c874608fbee9a76305f91d1cd96
jibreel <jibbo88@gmail.com>
2014-08-31 22:26:31 +0300

    models objects modifications

--- app/forum/models.py
+++ app/forum/models.py
@@ -1,4 +1,5 @@
 from app import redis
+from .auth import hash_pass
 
 
 class Error(Exception):
@@ -10,7 +11,7 @@ class UserExistsError(Error):
         self.msg = 'User already exists'
 
 
-class objects(object):
+class BaseModel(object):
 
     @classmethod
     def _create_id(cls, model=None):
@@ -32,7 +33,7 @@ class objects(object):
     @classmethod
     def _field_value_add(cls, field, value):
         """
-        check if value in -> key model:field
+        set value to -> key model:field
         used to store values where no fields can have the 
         same values.
         """
@@ -40,18 +41,45 @@ class objects(object):
         key = '{}:{}s'.format(cls.model, field)
         return redis.sadd(key, value)
 
+    @classmethod
+    def link_id(cls, field, _id, model=None):
+        """link a model field to id"""
+
+        model = model or cls.model
+        key = '{}:{}s'.format(model, model)
+        redis.hset(key, field, _id)
+
+    @classmethod
+    def get_id(cls, field, model=None):
+        """ get id based on a model's field value """
+
+        model = model or cls.model
+        key = '{}:{}s'.format(model, model)
+        return redis.hget(key, field)        
 
 
-
-class User(objects):
+class User(BaseModel):
     model = 'user'
 
     @classmethod
     def create(cls, username, password):
-        if cls._field_value_exists('username', username):
+        if cls.get_id(username):
             raise UserExistsError()
 
         key = cls._create_id()
-        redis.hmset(key, {'username': username, 'password': password})
-        cls._field_value_add('username', username)
-        return True
+        redis.hmset(key, {'username': username,
+                          'password': hash_pass(password)})
+
+        cls.link_id(username, key.split(':')[1])
+        return cls.get(key.split(':')[1])
+
+    @classmethod
+    def get(cls, _id):
+        """ get object by id """
+
+        key = '{}:{}'.format(cls.model, _id)
+        obj = redis.hgetall(key)
+        if obj:
+            obj['id'] = _id
+            return obj
+        return None
