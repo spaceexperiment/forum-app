c6fcabbf82f114e30fd26ec4fc8d177366795c53
jibreel <jibbo88@gmail.com>
2014-08-31 17:19:14 +0300

    auth.py for user authentication

---
+++ app/forum/auth.py
@@ -0,0 +1,15 @@
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
+from app import redis
+
+
+def register_user(username, password):
+    pass
+
+
+def login_user(username, password):
+    pass
+
+def logout_user(username):
+    pass
