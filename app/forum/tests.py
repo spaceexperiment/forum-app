3b26966463c0b0572ba5dfbdd01798d2fd237b42
jibreel <jibbo88@gmail.com>
2014-09-01 21:19:19 +0300

    some models testcases, mock & fakeredis >> requirements

--- app/forum/tests.py
+++ app/forum/tests.py
@@ -1,16 +1,50 @@
 import unittest
+import mock
+
+import fakeredis
+
+from . import models
 
 
-class UserTestCase(unittest.TestCase):
-
+class ModelTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        # self.redis = fakeredis.FakeStrictRedis()
+        self.patcher = mock.patch('app.forum.models.redis',
+                                  fakeredis.FakeStrictRedis())
+        self.patcher.start()
 
     def tearDown(self):
-        pass
+        # delete databases
+        fakeredis.DATABASES = {}
 
-    def test_create_account(self):
-        pass
+    def test_create_id(self):
+        key = models.BaseModel._create_id('model_name')
+        assert key == 'model_name:1'
 
-    def test_login_account(self):
-        pass
+        key = models.BaseModel._create_id('model_name')
+        assert key != 'model_name_obf:2'
+
+        key = models.BaseModel._create_id('model_name')
+        assert key != 'model_name:5'
+
+    def test_create_id_return_new_key(self):
+        key1 = models.BaseModel._create_id('model_name')
+        key2 = models.BaseModel._create_id('model_name')
+
+        assert key1 != key2
+
+
+
+
+# class UserTestCase(unittest.TestCase):
+
+#     def setUp(self):
+#         pass
+#     def tearDown(self):
+#         pass
+
+#     def test_create_account(self):
+#         pass
+
+#     def test_login_account(self):
+#         pass
