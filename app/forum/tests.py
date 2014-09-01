0e68d5d3931576edfd200b3a05da136e605c2cc8
jibreel <jibbo88@gmail.com>
2014-09-01 12:30:03 +0300

    test.py & piped nose

---
+++ app/forum/tests.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class UserTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_account(self):
+        pass
+
+    def test_login_account(self):
+        pass
