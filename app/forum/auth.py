d659ab089b4802ee5ea7d594ef476e07103da9d2
jibreel <jibbo88@gmail.com>
2014-09-02 22:46:09 +0300

    auth login logout

--- app/forum/auth.py
+++ app/forum/auth.py
@@ -1,13 +1,32 @@
 import os
+from base64 import b64encode
 
 from flask import request, session, g
 from werkzeug.security import check_password_hash
 
-from .models import User
+from .models import User, Session
+
+
+def _login_user(user):
+        session_key = b64encode(os.urandom(20))
+        # set uid in session:key
+        Session.set(session_key, user=user['id'])
+        # set session key in user object
+        User.set(user['id'], session=session_key)
+        # set session key in flask session
+        session['s_key'] = session_key
 
 
 def login_user(username, password):
-    pass
+    user = User.by_username(username)
+    if user and check_password_hash(user['password'], password):
+        _login_user(user)
+        return True
+    return False
 
-def logout_user(username):
-    pass
+
+def logout_user(user):
+    session_key = session.pop('s_key', None)
+    if session_key:
+        Session.delete(session_key)
+        User.delete_field(user['id'], 'session')
