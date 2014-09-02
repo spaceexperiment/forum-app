6d26c4b60604aa4ab7923b9f8f9fe14c78b3eb40
jibreel <jibbo88@gmail.com>
2014-09-02 22:48:30 +0300

    login logout logic in views

--- app/forum/views.py
+++ app/forum/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, request, session, g, redirect, url_for, abort, \
-                  render_template, flash, make_response
+                  render_template, flash
 
+from .models import User, Session, UserExistsError
 from .forms import RegisterForm
+from .auth import login_user, logout_user
+
 
 forum = Blueprint('forum', __name__)
 
@@ -9,7 +12,6 @@ forum = Blueprint('forum', __name__)
 @forum.route('/')
 def index():
     return render_template('forum.html')
-
 
 
 @forum.route('/user')
@@ -27,14 +29,30 @@ def login():
         repassword = form.repassword.data
 
         if repassword:
-            if password == repassword:
-                # create account
-                # login
-                flash('Account created')
-            form.errors['re-password'] = ['Password does not match']
-            return render_template('login.html', form=form)
-                
-        # login
+            if password != repassword:
+                form.errors['re-password'] = ['Password does not match']
+                return render_template('login.html', form=form)
+            try:
+                user = User.create(username, password)
+                flash('Account created', 'success')
+                login_user(username, password)
+                return redirect(url_for('.index'))
+            except UserExistsError:
+                form.errors['username'] = ['Username already exists']
+
+        if login_user(username, password):
+            return redirect(url_for('.index'))
+
+        form.errors['authentication'] = ['wrong username or password']
 
     return render_template('login.html', form=form)
 
+
+@forum.route('/logout')
+def logout():
+    session_key = session.get('s_key', None)
+    if session_key:
+        uid = Session.get(session_key)['user']
+        user = User.get(uid)
+        logout_user(user)
+    return redirect(url_for('.index'))
