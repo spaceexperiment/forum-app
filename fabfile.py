25a136f5fe3d1118f9e93e728a5cd645c1187e3e
jibreel <jibbo88@gmail.com>
2014-09-05 13:22:44 +0300

    typo

--- fabfile.py
+++ fabfile.py
@@ -2,5 +2,5 @@ from fabric.api import local
 
 
 def cover():
-    # nocse tests in current dir with coverage report
+    # nose tests in current dir with coverage report
     local('nosetests --with-coverage --cover-erase --cover-package=.')
