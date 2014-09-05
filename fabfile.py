f5b7f7b17cadcfbbc60fdd6001ad104448ced6c3
jibreel <jibbo88@gmail.com>
2014-09-05 13:31:49 +0300

    modified fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,3 +4,8 @@ from fabric.api import local
 def cover():
     # nose tests in current dir with coverage report
     local('nosetests --with-coverage --cover-erase --cover-package=.')
+
+
+def push():
+    cover()
+    local('git push origin master')
