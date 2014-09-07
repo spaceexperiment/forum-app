3ec95a7b31627d83382e47a28aa89317fcbfe408
jibreel <jibbo88@gmail.com>
2014-09-07 18:56:37 +0300

    structure.py typo

--- app/forum/structures.py
+++ app/forum/structures.py
@@ -2,11 +2,10 @@
 
 class AttrDict(dict):
     """
-    overided the getattr to return value from dict items instead
+    Overides the getattr to return value from dict items instead
     e.g.
     user = AttrDict({'username': 'marv'})
     user.username returns 'marv'
-
     """
 
     __getattr__ = dict.__getitem__
