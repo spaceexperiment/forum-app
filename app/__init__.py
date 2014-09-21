bcffb079a118a24fd4bc76f0cc4a7b3c8e5fdd72
jibreel <jibbo88@gmail.com>
2014-09-21 23:04:54 +0300

    add apu blueprint, json exception response

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, request, jsonify
+
+from werkzeug.exceptions import HTTPException, default_exceptions
+
 import redis
 
+redis = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 app = Flask('app')
 app.config.from_object('config')
 
-redis = redis.StrictRedis(host='localhost', port=6379, db=1)
+
+from app.api import api
+app.register_blueprint(api, url_prefix='/api')
 
 
-from app.client import forum
-app.register_blueprint(forum, url_prefix='/forum')
+def make_json_error(ex):
+    response = jsonify(message= str(ex))
+    response.status_code = ex.code if isinstance(ex, HTTPException) else 500
+    return response
+
+for code in default_exceptions.iterkeys():
+    app.error_handler_spec[None][code] = make_json_error
 
 
 @app.route('/')
