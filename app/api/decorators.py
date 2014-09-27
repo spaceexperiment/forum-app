43fd0e787c6b57fd5e3a980e6b593b554f9ee87b
jibreel <jibbo88@gmail.com>
2014-09-27 19:51:49 +0300

    refactored decorator

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import jsonify, make_response
+from flask import json, make_response
 
 
 def api_render(f):
@@ -8,6 +8,12 @@ def api_render(f):
     def decorated(*args, **kwargs):
         rv = f(*args, **kwargs)
         if isinstance(rv, tuple):
-            return make_response(jsonify(rv[0]), *rv[1:])
-        return make_response(jsonify(rv))
+            r = make_response(json.dumps(rv[0]))
+            r.status_code = rv[1]
+            if len(rv) == 3:
+                r.headers = rv[2]
+        else:
+            r = make_response(json.dumps(rv))
+        r.mimetype = 'application/json'
+        return r
     return decorated
